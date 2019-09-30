e51b5413edef98c2d3af01780d99fd56bd844b8b
asela <ecomsearchonline@gmail.com>
2019-10-01 03:04:25 +0530

    contact us page finished

--- src/containers/ContactUs/Form/ContactForm.js
+++ src/containers/ContactUs/Form/ContactForm.js
@@ -1,33 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import css from './ContactForm.css';
+import axios from 'axios';
 
-const ContactForm = props => {
 
-  return(
-    <div className={css.contacUsForm}>
-      <div>
-        <h2>Contact Us</h2>
-        <p>Swing by for a cup of coffee, or leave us a message:</p>
-      </div>
-      <div className="row">
-        <div className="column">
+class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      subject: '',
+      description: '',
+    };
+  }
 
+  send = () => {
+
+
+      axios.post('http://localhost:4000/api/v1/other/contactUs', {
+        name: JSON.stringify(this.state.name),
+        email: JSON.stringify(this.state.subject),
+        message: JSON.stringify(this.state.description),
+
+
+      }).then(
+        (response) => {
+          console.log(response)
+        }
+      )
+
+  }
+
+
+  render() {
+
+    return (
+      <div className={css.contacUsForm}>
+        <div>
+          <h2>Contact Us</h2>
+          <p>Swing by for a cup of coffee, or leave us a message:</p>
         </div>
-        <div className="column">
-          <form >
-            <label htmlFor="fname">First Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="Your name.."></input>
-            <label htmlFor="lname">Last Name</label>
-            <input type="text" id="lname" name="lastname" placeholder="Your last name.."></input>
+        <div className="row">
+          <div className="column">
 
-            <label htmlFor="subject">Subject</label>
-            <textarea id="subject" name="subject" placeholder="Write something.."
-            ></textarea>
-            <input type="submit" value="Submit"></input>
-          </form>
+          </div>
+          <div className="column">
+
+              <label htmlFor="fname">First Name</label>
+              <input value={this.state.name} onChange={(event) => { this.setState({ name: event.target.value }) }}
+                     type="text" id="fname" name="firstname" placeholder="Your name.."></input>
+              <label htmlFor="lname">subject</label>
+              <input value={this.state.subject}
+                          onChange={(event)=>{this.setState({subject:event.target.value})}}
+                          type="text" id="lname" name="subject" placeholder="subject"></input>
+
+              <label htmlFor="subject">Details</label>
+              <textarea value={this.state.description}
+                        onChange={(event)=>{this.setState({description:event.target.value})}}
+                        id="subject" name="description" placeholder="Write something.."
+              ></textarea>
+              <input onClick={() => {
+                this.send();
+              }} type="submit" value="Submit"></input>
+
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
 export default ContactForm;
