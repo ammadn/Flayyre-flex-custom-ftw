851e4cee9832ca53aa66ad50152617df1b47a74f
asela <ecomsearchonline@gmail.com>
2019-11-19 19:03:25 +0530

    add new logo to footer

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 
 import config from '../../config';
 import IconLogo from './IconLogo';
-import LogoImage from './FlayyreLogoWordS.png';
+import LogoImage from './../../assets/FlayyreNewLogo2[368].png';
 import css from './Logo.css';
 
 const Logo = props => {
