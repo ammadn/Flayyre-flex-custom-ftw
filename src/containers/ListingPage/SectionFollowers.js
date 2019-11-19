40ce76f3ed52ff92f26f1d25036e02ecd5e3698e
asela <ecomsearchonline@gmail.com>
2019-11-20 02:35:56 +0530

    change listing page

--- src/containers/ListingPage/SectionFollowers.js
+++ src/containers/ListingPage/SectionFollowers.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { array, shape, string } from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 
-import css from './ListingPage.css';
+import css from './SectionFollowers.css';
+import ImgFaceBook from '../../assets/facebook.png';
+import ImgTwiter from '../../assets/twitter.png';
+import ImgInstar from '../../assets/instagram.png';
+import Other from '../../assets/Other.png';
+import config from '../../config';
 
 const SectionFollowers = props => {
   const { publicData } = props;
@@ -16,18 +21,31 @@ const SectionFollowers = props => {
       <h2 className={css.capacityTitle}>
         <FormattedMessage id="sectionFollowers.title"/>
       </h2>
-      <p className={css.capacity}>followers</p>
-      <ul>
-        {publicData ?
-          <li>Fb    &#9; : {publicData.Fb}</li> : null
-        }
-        {publicData?
-          <li>Twitter &#9; : {publicData.Twitter}</li> : null}
-        {publicData ?
-          <li>IG     &#9; : {publicData.IG}</li> : null}
-        {publicData ?
-          <li>Other  &#9; : {publicData.Other}</li> : null}
-      </ul>
+
+      {publicData?
+        <div className={css.follower__group}>
+          <div className={css.follower}>
+            <img className={css.img__follower} src={ImgFaceBook}  />
+            <label>Facebook</label>
+            <label>{publicData.Fb}</label>
+          </div>
+          <div className={css.follower}>
+            <img className={css.img__follower} src={ImgTwiter}  />
+            <label>Twitter</label>
+            <label>{publicData.Twitter}</label>
+          </div>
+          <div className={css.follower}>
+            <img className={css.img__follower} src={ImgInstar}  />
+            <label>Instagram</label>
+            <label>{publicData.IG}</label>
+          </div>
+          <div className={css.follower}>
+            <img className={css.img__follower} src={Other}  />
+            <label>Other</label>
+            <label>{publicData.Other}</label>
+          </div>
+        </div>:null
+      }
     </div>
   );
 };
