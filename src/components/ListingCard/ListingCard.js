03926a061604408337419cd5f2f1f1b4247dddf5
asela <ecomsearchonline@gmail.com>
2020-05-03 01:15:51 +0530

    rating implemented

--- src/components/ListingCard/ListingCard.js
+++ src/components/ListingCard/ListingCard.js
@@ -65,6 +65,92 @@ export const ListingCardComponent = props => {
     : isDaily
       ? 'ListingCard.perDay'
       : 'ListingCard.perUnit';
+  console.log('props', props);
+
+  const filledStar = (<svg
+    className={css.fill_star}
+    viewBox="0 0 23 23" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M22.938 8.008c-.15-.412-.544-.69-.985-.69H14.38L12.507.758C12.377.31 11.967 0 11.5 0c-.467 0-.88.31-1.006.76L8.618 7.317H1.046c-.442 0-.833.278-.983.69-.15.414-.025.876.314 1.16l5.7 4.75L3.2 21.59c-.16.43-.02.916.346 1.196.362.28.87.29 1.242.02l6.71-4.79 6.713 4.79c.375.27.88.26 1.245-.02.366-.28.504-.765.343-1.196l-2.875-7.67 5.7-4.75c.34-.284.463-.746.315-1.16"
+      fill-rule="evenodd"></path>
+  </svg>);
+
+  const star = (<svg
+    className={css.star}
+    viewBox="0 0 23 23" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M22.938 8.008c-.15-.412-.544-.69-.985-.69H14.38L12.507.758C12.377.31 11.967 0 11.5 0c-.467 0-.88.31-1.006.76L8.618 7.317H1.046c-.442 0-.833.278-.983.69-.15.414-.025.876.314 1.16l5.7 4.75L3.2 21.59c-.16.43-.02.916.346 1.196.362.28.87.29 1.242.02l6.71-4.79 6.713 4.79c.375.27.88.26 1.245-.02.366-.28.504-.765.343-1.196l-2.875-7.67 5.7-4.75c.34-.284.463-.746.315-1.16"
+      fill-rule="evenodd"></path>
+  </svg>);
+
+
+  function rating() {
+    switch (Math.round(props.listing.attributes.metadata.rating)) {
+      case 1: {
+        var starts = [];
+        for (var i = 0; i < 1; i++) {
+          starts.push(filledStar);
+        }
+        for (var j = 0; j < 4; j++) {
+          starts.push(star);
+        }
+        return <span>{starts}</span>;
+      }
+        break;
+      case 2: {
+        var starts = [];
+        for (var i = 0; i < 2; i++) {
+          starts.push(filledStar);
+        }
+        for (var j = 0; j < 3; j++) {
+          starts.push(star);
+        }
+        return <span>{starts}</span>;
+      }
+        break;
+      case 3: {
+        var starts = [];
+        for (var i = 0; i < 3; i++) {
+          starts.push(filledStar);
+        }
+        for (var j = 0; j < 2; j++) {
+          starts.push(star);
+        }
+        return <span>{starts}</span>;
+      }
+        break;
+      case 4: {
+        var starts = [];
+        for (var i = 0; i < 4; i++) {
+          starts.push(filledStar);
+        }
+        for (var j = 0; j < 1; j++) {
+          starts.push(star);
+        }
+        return <span>{starts}</span>;
+      }
+        break;
+      case 5: {
+        var starts = [];
+        for (var i = 0; i < 5; i++) {
+          starts.push(filledStar);
+        }
+
+        return <span>{starts}</span>;
+      }
+        break;
+      case 6: {
+        var starts = [];
+        for (var i = 0; i < 5; i++) {
+          starts.push(filledStar);
+        }
+
+        return <span>{starts}</span>;
+      }
+        break;
+    }
+
+  }
 
   return (
     <NamedLink className={classes} name="ListingPage" params={{ id, slug }}>
@@ -86,9 +172,9 @@ export const ListingCardComponent = props => {
       <div className={css.info}>
         <div className={css.tags}>
           <div className={css.tagRoot}>
-            {listing.attributes.metadata['featured']?
+            {listing.attributes.metadata['featured'] ?
               <span
-                className={css.prmiumTag}><span>FEATURED</span></span>:null
+                className={css.prmiumTag}><span>FEATURED</span></span> : null
             }
             {/*<span*/}
             {/*className={css.verifiedTag}><span>VERIFIED</span></span>*/}
@@ -111,8 +197,14 @@ export const ListingCardComponent = props => {
                 longWordClass: css.longWord,
               })}
             </div>
-            <div className={css.authorInfo}>
-              <FormattedMessage id="ListingCard.hostedBy" values={{ authorName }}/>
+            <div className={css.rating_root}>
+              <div className={css.rating}>
+                {rating()}
+              </div>
+              <div className={css.authorInfo}>
+
+                <FormattedMessage id="ListingCard.hostedBy" values={{ authorName }}/>
+              </div>
             </div>
           </div>
         </div>
